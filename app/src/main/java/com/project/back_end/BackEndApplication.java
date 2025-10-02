b722b3fb5bae2e69b86ee0fd13bcbaafd5a5e9b7
priyanshu-shinde26 <priyanshu1983s@gmail.com>
2025-10-02 11:40:03 +0530

    github/workflows/ymlfiles

--- app/src/main/java/com/project/back_end/BackEndApplication.java
+++ app/src/main/java/com/project/back_end/BackEndApplication.java
@@ -2,14 +2,12 @@ package com.project.back_end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.project.back_end")
 public class BackEndApplication {
 
 	public static void main(String[] args) {
+		// This line starts the Spring application
 		SpringApplication.run(BackEndApplication.class, args);
 	}
-
 }
